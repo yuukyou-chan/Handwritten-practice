48326c07b384692b388f5583a1b9d8f332b81aa1
Yuukyou <chen.yongqiang@xingyungroup.com>
2025-04-25 15:33:18 +0800

    feat(算法): 添加最长无重复字符子串的解决方案
    添加滑动窗口和暴力解法两种方法来解决最长无重复字符子串问题。滑动窗口方法时间复杂度为 O(n)，而暴力解法时间复杂度为 O(n^2)，无法通过 LeetCode 测试。

---
+++ 算法/最长无重复字串.js
@@ -0,0 +1,48 @@
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
+ * 示例 1:
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ */
+
+// 滑动窗口
+function lengthOfLongestSubstring(s) {
+  const charIndexMap = new Map();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+    if (
+      charIndexMap.has(currentChar) &&
+      charIndexMap.get(currentChar) >= left
+    ) {
+      left = charIndexMap.get(currentChar) + 1;
+    }
+    charIndexMap.set(currentChar, right);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+}
+
+// 暴力解法 时间复杂度 O(n^2) 无法通过 leetCode
+function lengthOfLongestSubstring(s) {
+  let maxString = "";
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i; j < s.length; j++) {
+      let currString = s.slice(i, j + 1);
+      let currCharacter = s[j + 1];
+      console.log(currString, currCharacter, i, j);
+      if (currString.length > maxString.length) {
+        maxString = currString;
+      }
+      if (currString.includes(currCharacter)) {
+        break;
+      }
+    }
+  }
+
+  return maxString.length;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
