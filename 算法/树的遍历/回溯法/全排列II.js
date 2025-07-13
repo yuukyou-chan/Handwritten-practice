f427f07d1389e6e0333b68b023e3de20e821d473
yuukyou <1968159353@qq.com>
2025-07-13 18:23:51 +0800

    feat(回溯算法): 添加全排列II的解法并更新文档
    添加处理重复元素的全排列算法实现，并在文档中补充去重操作的说明

---
+++ 算法/树的遍历/回溯法/全排列II.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function (nums) {
+  const path = [];
+  const res = [];
+  const used = [];
+  nums.sort((a, b) => a - b);
+
+  function backTracing(path, start) {
+    if (path.length === nums.length) {
+      res.push([...path]);
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      const currNum = nums[i];
+
+      // nums[i] === nums[i-1] && !used[i-1] 进行去重；!used[i-1] 是为了进行树层去重，避免走到树枝
+      if (used[i] || (nums[i] === nums[i - 1] && !used[i - 1])) {
+        continue;
+      }
+      path.push(currNum);
+      used[i] = true;
+      backTracing(path, i + 1);
+      path.pop();
+      used[i] = false;
+    }
+  }
+  backTracing(path, 0);
+  return res;
+};
