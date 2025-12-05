c603e32b96f8b3366055e318a7043b34e8e781e3
Yuukyou <1968159353@qq.com>
2025-12-05 14:40:06 +0800

    feat(请求缓存控制器): 实现缓存请求控制逻辑
    添加请求缓存控制器的核心功能，包括缓存管理和并发请求处理
    当相同请求并发时，确保后续请求等待已有请求完成
    请求结果会被缓存以供后续使用

--- 手写工具函数/请求缓存控制器.js
+++ 手写工具函数/请求缓存控制器.js
@@ -3,12 +3,35 @@
 // 2. 需要考虑a页面请求还没结束，b页面又发起了请求，此时b页面的请求应该等待a页面的请求结果返回后再返回
 
 class CacheRequestController {
-  constructor() {
+  constructor(requestor) {
     // 存放缓存数据
     this.cache = new Map();
     // 记录正在请求的数据
     this.pendingRequest = new Map();
+    //
+    this.requestor = requestor;
   }
 
-  async cacheFetch(url, options) {}
+  async cacheFetch(url, option) {
+    let res;
+    const currCache = this.cache.get(url + option);
+    if (currCache) return currCache;
+
+    const currPromise = this.pendingRequest.get(url + option);
+    if (currPromise) {
+      res = currPromise;
+    } else {
+      res = this.requestor.fetch(url, option);
+      this.pendingRequest.set(url + option, res);
+      res
+        .then((result) => {
+          this.cache.set(url + option, result);
+        })
+        .finally(() => {
+          this.pendingRequest.delete(url + option);
+        });
+    }
+
+    return res;
+  }
 }
